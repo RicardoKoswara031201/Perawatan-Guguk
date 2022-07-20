d7ff0a250d4ecc6f3b59ba1d5b7b9f0205a6273b
Asus <Asus@DESKTOP-245R2UA>
2022-07-20 18:30:26 +0700

    menambah Cabang

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -12,34 +12,44 @@ package Model;
 public class Admin {
     private String username;
     private String password;
+    private String idCabang;
 
-    public Admin(String username, String password) {
+    public Admin(String username, String password, String idCabang) {
         this.username = username;
         this.password = password;
+        this.idCabang = idCabang;
     }
     
     public Admin(){
         
     }
-    
+
     public String getUsername() {
         return username;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getIdCabang() {
+        return idCabang;
+    }
+
+    public void setIdCabang(String idCabang) {
+        this.idCabang = idCabang;
     }
     
     @Override
     public String toString() {
-        return "Admin {" + "username=" + username + ", password=" + password + "}";
+        return "Admin{" + "username=" + username + ", password=" + password + ", idCabang=" + idCabang + '}';
     }
 }
